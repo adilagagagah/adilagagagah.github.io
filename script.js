445ffff399313a26475cfcc6bc9201de83275e2a
Gagah Adilaga <gagahpusokoadilaga03@gmail.com>
2023-12-30 02:47:34 +0700

    menambahkan fungsi untuk scroll saat navbar diklik

---
+++ script.js
@@ -0,0 +1,14 @@
+// Fungsi untuk menangani klik pada tautan
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Menambahkan event listener untuk setiap tautan
+document.querySelectorAll('a').forEach(function (link) {
+    link.addEventListener('click', function (event) {
+        event.preventDefault(); // Mencegah perilaku default dari tautan
+        var targetSectionId = link.getAttribute('href').substring(1); // Mengambil id section dari href
+        scrollToSection(targetSectionId);
+    });
+});
