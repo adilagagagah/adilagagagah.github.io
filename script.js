9db0b725f0394b97da891812a6d5b8a845cf1fec
Gagah Adilaga <gagahpusokoadilaga03@gmail.com>
2023-12-30 12:47:43 +0700

    merubah nama element id untuk about, portofolio, dan contact

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function scrollToSection(sectionId) {
 // Menambahkan event listener untuk setiap tautan
 document.querySelectorAll('a').forEach(function (link) {
     link.addEventListener('click', function (event) {
-        // event.preventDefault(); // Mencegah perilaku default dari tautan
+        event.preventDefault(); // Mencegah perilaku default dari tautan
         var targetSectionId = link.getAttribute('href').substring(1); // Mengambil id section dari href
         scrollToSection(targetSectionId);
     });
