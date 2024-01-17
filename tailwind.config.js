c2e806a33ae0c9864cf498dd3767a72a2dfe0b72
Gagah Adilaga <gagahpusokoadilaga03@gmail.com>
2024-01-17 21:03:59 +0700

    menambahkan home sebelah kiri

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#14b8a6'
+        primary: '#06b6d4', //cyan-500
+        dark: '#0f172a', //slate-900
       },
       screens: {
         '2xl': '1320px',
